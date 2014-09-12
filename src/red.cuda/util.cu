cbf31935f4c22f19dedb6de728df5488094b88bc
suliaron <draronsuli@gmail.com>
2014-09-12 14:47:38 +0200

    Initial check-in

---
+++ src/red.cuda/util.cu
@@ -0,0 +1,35 @@
+#include "util.h"
+#include "nbody_exception.h"
+#include "red_type.h"
+#include "red_macro.h"
+
+void allocate_device_vector(void **d_ptr, size_t size, const char *file, int line)
+{
+	cudaError_t cudaStatus = cudaSuccess;
+	cudaMalloc(d_ptr, size);
+	cudaStatus = HandleError(cudaStatus, file, line);
+	if (cudaSuccess != cudaStatus)
+	{
+		throw nbody_exception("cudaMalloc failed", cudaStatus);
+	}
+}
+
+void copy_vector_to_device(void* dst, const void *src, size_t count)
+{
+	cudaMemcpy(dst, src, count, cudaMemcpyHostToDevice);
+	cudaError_t cudaStatus = HANDLE_ERROR(cudaGetLastError());
+	if (cudaSuccess != cudaStatus)
+	{
+		throw nbody_exception("cudaMemcpy failed (copy_vector_to_device)", cudaStatus);
+	}
+}
+
+void copy_vector_to_host(void* dst, const void *src, size_t count)
+{
+	cudaMemcpy(dst, src, count, cudaMemcpyDeviceToHost);
+	cudaError_t cudaStatus = HANDLE_ERROR(cudaGetLastError());
+	if (cudaSuccess != cudaStatus)
+	{
+		throw nbody_exception("cudaMemcpy failed (copy_vector_to_host)", cudaStatus);
+	}
+}
---
+++ src/red.cuda/util.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "red_type.h"
+
+void allocate_device_vector(void **d_ptr, size_t size, const char *file, int line);
+#define ALLOCATE_DEVICE_VECTOR(d_ptr, size) (allocate_device_vector(d_ptr, size, __FILE__, __LINE__))
+
+void copy_vector_to_device(void* dst, const void *src, size_t count);
+void copy_vector_to_host(void* dst, const void *src, size_t count);
