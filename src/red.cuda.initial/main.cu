8952b6ec62e9026691c1b93d33f1ba1fb63ef89c
suliaron <draronsuli@gmail.com>
2014-11-14 14:47:26 +0100

    Initial check-in

--- src/red.cuda.initial/main.cu
+++ src/red.cuda.initial/main.cu
@@ -1,7 +1,8 @@
-// includes, system 
+﻿// includes, system 
 #include <algorithm>
 #include <cmath>
 #include <ctime>
+#include <cassert>
 #include <stdint.h>
 #include <cstdlib>
 #include <fstream>
@@ -12,40 +13,221 @@
 #include <vector>
 
 // includes, project
-//#include "pp_disk.h"
 #include "red_constants.h"
 #include "red_type.h"
 #include "redutilcu.h"
-
-#define MASS_STAR		1.0			// M_sol
-#define MASS_JUPITER	1.0e-3		// M_sol
-#define RAD_STAR		0.005		// AU
-
-#define MASS_SUN		1.9891E+30	// kg
-#define MASS_FACTOR		5e-7		// M_sol
-#define MASS_MU			log(4.0)
-#define MASS_S			0.3
-#define MASS_MIN		1.0e-20		// M_sol
-#define MASS_MAX		1.0e-19		// M_sol
-
-#define DIST_MIN		4.5			// AU
-#define DIST_MAX		15			// AU
-
-//#define DENSITY			3000.0		// kg m-3
-#define AU				149.6e9		// m
-
-// It must be enclosed in parentheses in order to give correct results in
-// the case of a division i.e. 1/SQR(x) -> 1/((x)*(x))
-#define	SQR(x)			((x)*(x))
-#define	CUBE(x)			((x)*(x)*(x))
-
-typedef double ioT;
 
 using namespace std;
 using namespace redutilcu;
 
 
 static string body_type_names[] = {"star", "giant", "rocky", "proto", "superpl", "pl", "testp"}; 
+
+namespace random_generator
+{
+	class red_random
+	{
+	public:
+		red_random(unsigned int seed);
+		~red_random();
+
+		unsigned int rand();
+
+		var_t uniform();
+		int   uniform(int a, int b);
+		var_t uniform(var_t a, var_t b);
+
+		var_t normal(var_t m, var_t s);
+		var_t exponential(var_t lambda);
+		var_t rayleigh(var_t sigma);
+
+		var_t power_law(var_t lower, var_t upper, var_t p);
+
+	private:
+		unsigned int idx;
+		unsigned int I[624];
+	};
+
+
+	red_random::red_random(unsigned int seed) :
+		idx(0)
+	{
+		I[  0] = seed & 0xffffffff;
+		for(int i = 1; i < 624; ++i) 
+		{
+			I[i] = (1812433253*(I[i-1]^I[i-1]>>30)+i)&0xffffffff;
+		}
+	}
+
+	red_random::~red_random()
+	{ } 
+
+	unsigned int red_random::rand()
+	{
+		unsigned int j = idx < 623 ? idx + 1 : 0;
+		unsigned int y = I[idx]&0x80000000 | I[j]&0x7fffffff;
+		y = I[idx] = I[idx < 227 ? idx + 397 : idx-227]^y>>1^(y&1)*0x9908b0df;
+		idx = j;
+		return y^(y^=(y^=(y^=y>>11)<<7&0x9d2c5680)<<15&0xefc60000)>>18;
+	}
+
+	var_t red_random::uniform()
+    {
+        return ((rand() + 1.0) / 4294967296.0);
+    }
+
+	int red_random::uniform(int a, int b)
+    {
+        return (a + (unsigned int)(rand()/4294967296.*(b - a + 1)));
+    }
+
+	var_t red_random::uniform(var_t a, var_t b)
+    {
+        return (a + (b - a)*(rand() + 1.0) / 4294967296.0);
+    }
+
+	var_t red_random::normal(var_t m, var_t s)
+    {
+        return (m + s*sqrt(-2.0*log((rand() + 1.0) / 4294967296.0)) * cos(1.4629180792671596E-9*(rand() + 1.0)));
+    }
+
+	var_t red_random::exponential(var_t lambda)
+    {
+        var_t u = uniform(); //(rand() + 1.0)/4294967296.0;
+        return (-1.0 / lambda * log(u));
+    }
+
+    var_t red_random::rayleigh(var_t sigma)
+    {
+        var_t u = uniform();//(rand() + 1.0)/4294967296.0;
+        return (sigma * sqrt(-2.0 * log(u)));
+    }
+
+	var_t red_random::power_law(var_t x_min, var_t x_max, var_t p)
+	{
+		assert(x_min < x_max);
+
+		var_t y_min = pow(x_min, p);
+		var_t y_max = pow(x_max, p);
+		if (y_min > y_max)
+		{
+			swap(y_min, y_max);
+		}
+
+		var_t d_y = y_max - y_min;
+		var_t d_x = x_max - x_min;
+
+		var_t x, y;
+		var_t area_max = d_x * d_y;
+
+		do
+		{
+			x = uniform(0.0, area_max) / d_y + x_min;
+			y = uniform(y_min, y_max);
+		} while (y > pow(x, p));
+
+		return x;
+	}
+
+
+
+
+
+
+	class pdf_base
+	{
+	public:
+		pdf_base();
+		~pdf_base();
+
+		virtual var_t get_value(var_t x) = 0;
+	};
+
+	class pdf_constant : public pdf_base
+	{
+	public:
+		pdf_constant();
+		~pdf_constant();
+
+		var_t get_value(var_t x);
+	};
+
+	class pdf_exponential : public pdf_base
+	{
+	public:
+		pdf_exponential(var_t lambda);
+		~pdf_exponential();
+
+		var_t get_value(var_t x);
+	private:
+		var_t lambda;
+	};
+
+	pdf_base::pdf_base()
+	{ }
+
+	pdf_base::~pdf_base()
+	{ }
+
+	pdf_constant::pdf_constant()
+	{ }
+
+	pdf_constant::~pdf_constant()
+	{ }
+
+	var_t pdf_constant::get_value(var_t x)
+	{
+		return 1.0;
+	}
+
+	pdf_exponential::pdf_exponential(var_t lambda) :
+		pdf_base(),
+		lambda(lambda)
+	{ }
+
+	pdf_exponential::~pdf_exponential()
+	{ }
+
+	var_t pdf_exponential::get_value(var_t x)
+	{
+		return (lambda * exp(-lambda * x));
+	}
+
+	var_t generate(var2_t range, pdf_base* pdf)
+	{
+		var_t x;
+		var_t y;
+
+		do
+		{
+			x = range.x + (var_t)rand() / RAND_MAX * (range.y - range.x);
+			y = (var_t)rand() / RAND_MAX;
+		}
+		while (y > pdf->get_value(x));
+
+		return x;
+	}
+
+	void print_data(ostream& sout, vector<var_t>& data)
+	{
+		static int var_t_w = 17;
+
+		sout.precision(8);
+		sout.setf(ios::right);
+		sout.setf(ios::scientific);
+
+		for (int i = 0; i < data.size(); i++)
+		{
+			sout << setw(var_t_w) << data[i] << endl;
+		}
+
+		sout.flush();
+	}
+
+
+} /* random_generator */
+
+
 
 typedef struct orbelem
 		{
@@ -84,8 +266,10 @@ typedef enum phys_prop_name
 
 typedef struct distribution
 		{
-			var2_t	limits;
-			var_t	(*pdf)(var_t);
+			var2_t	range;
+			var_t*	params;
+			var_t	(*pdf)(var_t x);
+
 		} distribution_t;
 
 typedef struct oe_dist
@@ -109,41 +293,6 @@ typedef struct body_disk
 		} body_disk_t;
 
 
-// Draw a number from a given distribution
-var_t generate_random(var_t xmin, var_t xmax, var_t p(var_t))
-{
-	var_t x;
-	var_t y;
-
-	do
-	{
-		x = xmin + (var_t)rand() / RAND_MAX * (xmax - xmin);
-		y = (var_t)rand() / RAND_MAX;
-	}
-	while (y > p(x));
-
-	return x;
-}
-
-var_t pdf_mass_lognormal(var_t x)
-{
-	return 1.0 / sqrt(2 * PI) / MASS_S * exp(-pow(log(x) - MASS_MU, 2) / 2 / MASS_S / MASS_MU);
-}
-
-var_t pdf_distance_squared(var_t d)
-{
-	return d * d / DIST_MAX / DIST_MAX;
-}
-
-var_t pdf_distance_exp(var_t d)
-{
-	return exp(-d) * d * d;
-}
-
-var_t pdf_const(var_t x)
-{
-	return 1;
-}
 
 #define FOUR_PI_OVER_THREE	4.1887902047863909846168578443727
 var_t calculate_radius(var_t m, var_t density)
@@ -262,6 +411,7 @@ void print_body_record(
     output.flush();
 }
 
+typedef double ioT;
 void Emese_data_format_to_red_cuda_format(const string& input_path, const string& output_path)
 {
 	ifstream input(input_path.c_str(), ios::in | ios::binary);
@@ -365,16 +515,19 @@ void Emese_data_format_to_red_cuda_format(const string& input_path, const string
 	}
 }
 
-void set(distribution_t& d, var_t x, var_t (*pdf)(var_t))
+void set(distribution_t& d, var_t x0, var_t (*pdf)(var_t))
 {
-	d.limits.x = d.limits.y = x;
+	d.range.x = d.range.y = x0;
 	d.pdf = pdf;
 }
 
-void set(distribution_t& d, var_t x, var_t y, var_t (*pdf)(var_t))
+void set(distribution_t& d, var_t x0, var_t x1, var_t* pdf_params, var_t (*pdf)(var_t))
 {
-	d.limits.x = x;
-	d.limits.y = y;
+	assert(x0 < x1);
+
+	d.range.x = x0;
+	d.range.y = x1;
+	d.params = pdf_params;
 	d.pdf = pdf;
 }
 
@@ -398,11 +551,11 @@ void set_default(body_disk &bd)
 		bd.nBody[body_type] = 0;
         for (int i = 0; i < ORBITAL_ELEMENT_N; i++) 
 		{
-			set(bd.oe_d[body_type].item[i], 0.0, 0.0, pdf_const);
+			set(0.0, 0x0, pdf_const, bd.oe_d[body_type].item[i]);
 		}
 		for (int i = 0; i < 4; i++) 
 		{
-			set(bd.pp_d[body_type].item[i], 0.0, 0.0, pdf_const);
+			set(0.0, 0x0, pdf_const, bd.pp_d[body_type].item[i]);
 		}
 	}
 }
@@ -419,43 +572,43 @@ int_t	calculate_number_of_bodies(body_disk &bd)
 
 void generate_oe(oe_dist_t *oe_d, orbelem_t& oe)
 {
-    oe.sma  = generate_random(oe_d->item[ORBITAL_ELEMENT_SMA].limits.x,  oe_d->item[ORBITAL_ELEMENT_SMA].limits.y,  oe_d->item[ORBITAL_ELEMENT_SMA].pdf);
-	oe.ecc  = generate_random(oe_d->item[ORBITAL_ELEMENT_ECC].limits.x,  oe_d->item[ORBITAL_ELEMENT_ECC].limits.y,  oe_d->item[ORBITAL_ELEMENT_ECC].pdf);
-	oe.inc  = generate_random(oe_d->item[ORBITAL_ELEMENT_INC].limits.x,  oe_d->item[ORBITAL_ELEMENT_INC].limits.y,  oe_d->item[ORBITAL_ELEMENT_INC].pdf);
-	oe.peri = generate_random(oe_d->item[ORBITAL_ELEMENT_PERI].limits.x, oe_d->item[ORBITAL_ELEMENT_PERI].limits.y, oe_d->item[ORBITAL_ELEMENT_PERI].pdf);
-	oe.node = generate_random(oe_d->item[ORBITAL_ELEMENT_NODE].limits.x, oe_d->item[ORBITAL_ELEMENT_NODE].limits.y, oe_d->item[ORBITAL_ELEMENT_NODE].pdf);
-    oe.mean = generate_random(oe_d->item[ORBITAL_ELEMENT_MEAN].limits.x, oe_d->item[ORBITAL_ELEMENT_MEAN].limits.y, oe_d->item[ORBITAL_ELEMENT_MEAN].pdf);
+    oe.sma  = generate_random(oe_d->item[ORBITAL_ELEMENT_SMA].range.x,  oe_d->item[ORBITAL_ELEMENT_SMA].range.y,  oe_d->item[ORBITAL_ELEMENT_SMA].pdf);
+	oe.ecc  = generate_random(oe_d->item[ORBITAL_ELEMENT_ECC].range.x,  oe_d->item[ORBITAL_ELEMENT_ECC].range.y,  oe_d->item[ORBITAL_ELEMENT_ECC].pdf);
+	oe.inc  = generate_random(oe_d->item[ORBITAL_ELEMENT_INC].range.x,  oe_d->item[ORBITAL_ELEMENT_INC].range.y,  oe_d->item[ORBITAL_ELEMENT_INC].pdf);
+	oe.peri = generate_random(oe_d->item[ORBITAL_ELEMENT_PERI].range.x, oe_d->item[ORBITAL_ELEMENT_PERI].range.y, oe_d->item[ORBITAL_ELEMENT_PERI].pdf);
+	oe.node = generate_random(oe_d->item[ORBITAL_ELEMENT_NODE].range.x, oe_d->item[ORBITAL_ELEMENT_NODE].range.y, oe_d->item[ORBITAL_ELEMENT_NODE].pdf);
+    oe.mean = generate_random(oe_d->item[ORBITAL_ELEMENT_MEAN].range.x, oe_d->item[ORBITAL_ELEMENT_MEAN].range.y, oe_d->item[ORBITAL_ELEMENT_MEAN].pdf);
 }
 
 void generate_pp(phys_prop_dist_t *pp_d, param_t& param)
 {
-	param.mass = generate_random(pp_d->item[MASS].limits.x, pp_d->item[MASS].limits.y, pp_d->item[MASS].pdf);
+	param.mass = generate_random(pp_d->item[MASS].range.x, pp_d->item[MASS].range.y, pp_d->item[MASS].pdf);
 
-	if (	 pp_d->item[DENSITY].limits.x == 0.0 && pp_d->item[DENSITY].limits.y == 0.0 &&
-			 pp_d->item[RADIUS].limits.x == 0.0 && pp_d->item[RADIUS].limits.y == 0.0 )
+	if (	 pp_d->item[DENSITY].range.x == 0.0 && pp_d->item[DENSITY].range.y == 0.0 &&
+			 pp_d->item[RADIUS].range.x == 0.0 && pp_d->item[RADIUS].range.y == 0.0 )
 	{
 		param.radius = 0.0;
 		param.density = 0.0;
 	}
-	else if (pp_d->item[DENSITY].limits.x == 0.0 && pp_d->item[DENSITY].limits.y == 0.0 &&
-			 pp_d->item[RADIUS].limits.x > 0.0 && pp_d->item[RADIUS].limits.y > 0.0 )
+	else if (pp_d->item[DENSITY].range.x == 0.0 && pp_d->item[DENSITY].range.y == 0.0 &&
+			 pp_d->item[RADIUS].range.x > 0.0 && pp_d->item[RADIUS].range.y > 0.0 )
 	{
-		param.radius = generate_random(pp_d->item[RADIUS].limits.x, pp_d->item[RADIUS].limits.y, pp_d->item[RADIUS].pdf);
+		param.radius = generate_random(pp_d->item[RADIUS].range.x, pp_d->item[RADIUS].range.y, pp_d->item[RADIUS].pdf);
 		param.density = calculate_density(param.mass, param.radius);
 	}
-	else if (pp_d->item[DENSITY].limits.x > 0.0 && pp_d->item[DENSITY].limits.y > 0.0 &&
-			 pp_d->item[RADIUS].limits.x == 0.0 && pp_d->item[RADIUS].limits.y == 0.0 )
+	else if (pp_d->item[DENSITY].range.x > 0.0 && pp_d->item[DENSITY].range.y > 0.0 &&
+			 pp_d->item[RADIUS].range.x == 0.0 && pp_d->item[RADIUS].range.y == 0.0 )
 	{
-		param.density = generate_random(pp_d->item[DENSITY].limits.x, pp_d->item[DENSITY].limits.y, pp_d->item[DENSITY].pdf);
+		param.density = generate_random(pp_d->item[DENSITY].range.x, pp_d->item[DENSITY].range.y, pp_d->item[DENSITY].pdf);
 		param.radius = calculate_radius(param.mass, param.density);
 	}
 	else
 	{
-		param.radius = generate_random(pp_d->item[RADIUS].limits.x, pp_d->item[RADIUS].limits.y, pp_d->item[RADIUS].pdf);
-		param.density = generate_random(pp_d->item[DENSITY].limits.x, pp_d->item[DENSITY].limits.y, pp_d->item[DENSITY].pdf);
+		param.radius = generate_random(pp_d->item[RADIUS].range.x, pp_d->item[RADIUS].range.y, pp_d->item[RADIUS].pdf);
+		param.density = generate_random(pp_d->item[DENSITY].range.x, pp_d->item[DENSITY].range.y, pp_d->item[DENSITY].pdf);
 	}
 
-	param.cd = generate_random(pp_d->item[DRAG_COEFF].limits.x, pp_d->item[DRAG_COEFF].limits.y, pp_d->item[DRAG_COEFF].pdf);
+	param.cd = generate_random(pp_d->item[DRAG_COEFF].range.x, pp_d->item[DRAG_COEFF].range.y, pp_d->item[DRAG_COEFF].pdf);
 }
 
 int generate_pp_disk(const string &path, body_disk_t& body_disk)
@@ -556,7 +709,7 @@ void set_parameters_of_Two_body_disk(body_disk_t& disk)
 	int type = BODY_TYPE_STAR;
 
 	disk.names.push_back("star");
-	set(disk.pp_d[type].item[MASS], 1.0, pdf_const);
+	set(1.0, 0x0, pdf_const, disk.pp_d[type].item[MASS]);
 	set(disk.pp_d[type].item[RADIUS], 1.0 * constants::SolarRadiusToAu, pdf_const);
 	set(disk.pp_d[type].item[DRAG_COEFF], 0.0, pdf_const);
 	disk.mig_type[bodyIdx] = MIGRATION_TYPE_NO;
@@ -787,6 +940,68 @@ int parse_options(int argc, const char **argv, string &outDir, string &filename)
 //-o C:\Work\Projects\red.cuda\TestRun\DvorakDisk\Run_cf_5 -f Run_cf_5.txt
 int main(int argc, const char **argv)
 {
+	/*
+	 * Test the red_random class
+	 */
+
+	{
+		const int N = 10000;
+		string dir = "C:\\Work\\Projects\\red.cuda\\TestRandomGenerator\\";
+		try
+		{
+			string path = dir + "power_law_-1.5.txt";
+			ostream* data_f = new ofstream(path.c_str(), ios::out);
+
+			random_generator::red_random my_random(1);
+
+			vector<var_t>* data = new vector<var_t>[N];
+			for (int i = 0; i < N; i++)
+			{
+				data->push_back(my_random.power_law(0.4, 4.0, -1.5));
+			}
+			random_generator::print_data(*data_f, *data);
+		}
+		catch (const exception ex)
+		{
+			cerr << "Error: " << ex.what() << endl;
+		}
+
+		return 0;
+	}
+
+
+	/*
+	 * Test the random_generator namespace
+	 */
+	{
+		const int N = 10000;
+		string dir = "C:\\Work\\Projects\\red.cuda\\TestRandomGenerator\\";
+		try
+		{
+			string path = dir + "pdf_exponential_10000.txt";
+			ostream* data_f = new ofstream(path.c_str(), ios::out);
+
+			var2_t range = {0.0, 5.0};
+			random_generator::pdf_base* pdf_exp = new random_generator::pdf_exponential(2.0);
+
+			vector<var_t>* data = new vector<var_t>[N];
+
+			for (int i = 0; i < N; i++)
+			{
+				data->push_back(random_generator::generate(range, pdf_exp));
+			}
+			random_generator::print_data(*data_f, *data);
+
+		}
+		catch (const exception ex)
+		{
+			cerr << "Error: " << ex.what() << endl;
+		}
+
+		return 0;
+	}
+
+
 	body_disk_t disk;
 	string outDir;
 	string filename;
